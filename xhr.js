c2704d635e4d82514704ff149e5b5e818adabc59
shivam <shivampatel27102001@gmail.com>
2021-01-29 18:36:52 +0530

    post request

--- xhr.js
+++ xhr.js
@@ -1,20 +1,41 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET','https://reqres.in/api/users?page=2'); 
+const sendHttpRequest = (method,url,data) => {
 
+    const promise = new Promise((resolve,reject) => {
+        const xhr = new XMLHttpRequest();
+    xhr.open(method, url); 
+
+    xhr.responseType = 'json';
+
+    if(data){ 
+    xhr.setRequestHeader('Content-Type','application/json');
+}
     xhr.onload = () => {
-        const data = JSON.parse(xhr.response);
-        console.log(data);  
+        resolve(xhr.response); 
     };
 
 
-    xhr.send(); // send request
+    xhr.send(JSON.stringify(data));
+    });
+    return promise;
 };
 
-const sendData = () => {};
+const getData = () => {
+    sendHttpRequest('GET','https://reqres.in/api/users?page=2').then(responseData => {
+         console.log(responseData);
+    });
+};
+
+const sendData = () => {
+    sendHttpRequest('POST','https://reqres.in/api/register',{
+        email : 'eve.holt@reqres.in',
+        password : 'pistol'
+    }).then(responseData => {
+        console.log(responseData);
+    });
+};
 
 getBtn.addEventListener('click',getData);
 postBtn.addEventListener('click',sendData);
